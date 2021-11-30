8632bb8d0064138c1ae8bb4a37ee87ab48f9e0dd
초급개발 승히 <79691180+good-luck-sh@users.noreply.github.com>
2021-11-30 18:26:32 +0900

    add commit

---
+++ src/main/java/dao/UserPointHistoryDao.java
@@ -0,0 +1,34 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import dto.UserDto;
+
+import static utils.ConnectionUtil.*;
+
+public class UserPointHistoryDao {
+	
+	
+	package dao;
+
+	import java.util.List;
+
+	import vo.UserPointHistory;
+
+	public class UserPointHistoryDao {
+		
+		private static UserPointHistoryDao self = new UserPointHistoryDao();
+		private UserPointHistoryDao() {}
+		public static UserPointHistoryDao getInstance() {
+			return self;
+
+		
+		
+
+	}
+}
