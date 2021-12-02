91a8f0ff87c8fb3e04591c83d86a197f0730dfcb
YUSANGHYO <godjilla6871@naver.com>
2021-12-02 18:09:52 +0900

    상품 정보 수정하기 기능 추가

--- src/main/java/dao/ProductDao.java
+++ src/main/java/dao/ProductDao.java
@@ -14,157 +14,166 @@ import vo.Product;
 
 public class ProductDao {
 	
-	public List<Product> getBestProducts() throws SQLException {
-		List<Product> productList = new ArrayList<>();
+	public void deleteProduct(int productNo) throws SQLException {
+		String sql = "delete "
+					+"from product "
+					+"where product_no = ? ";
 		
-		return productList;
+		Connection connection = getConnection();
+	    PreparedStatement pstmt = connection.prepareStatement(sql);
+	    pstmt.setInt(1, productNo);
+	    pstmt.executeQuery();
+	    
+	    pstmt.close();
+	    connection.close();
 	}
 	
-	
 	/**
-	 * 해당상품번호에 해당하는 상품정보 가져오기
-	 * @param productNo
-	 * @return
-	 * @throws SQLException
-	 */
-	public List<Product> selectProductByProductNo(int productNo) throws SQLException {
-		String sql = "select product_no , category_no, product_name, product_price, "
-				+ "product_discount_price, product_stock, product_on_sale, product_picture "
-				+ "from product "
-				+ "where product_no = ? "
-				+ "order by product_no asc ";
-		
-		List<Product> productList = new ArrayList<>();
+	    * 해당상품번호에 해당하는 상품정보 가져오기
+	    * @param productNo
+	    * @return
+	    * @throws SQLException
+	    */
+	   public List<Product> selectProductByProductNo(int productNo) throws SQLException {
+	      String sql = "select product_no , category_no, product_name, product_price, "
+	            + "product_discount_price, product_stock, product_on_sale, product_picture "
+	            + "from product "
+	            + "where product_no = ? "
+	            + "order by product_no asc ";
+	      
+	      List<Product> productList = new ArrayList<>();
+	      
+	      Connection connection = getConnection();
+	      PreparedStatement pstmt = connection.prepareStatement(sql);
+	      pstmt.setInt(1, productNo);
+	      ResultSet rs = pstmt.executeQuery();
+	      
+	      while(rs.next()) {
+	         Product product = new Product();
+	         Categorys category = new Categorys();
+	         
+	         product.setProductNo(rs.getInt("product_no"));
+	         product.setProductName(rs.getString("product_name"));
+	         product.setProductPrice(rs.getInt("product_price"));
+	         product.setProductDiscountPrice(rs.getInt("product_discount_price"));
+	         product.setProductStock(rs.getInt("product_stock"));
+	         product.setProductOnSale(rs.getString("product_on_sale"));
+	         product.setProductPicture(rs.getString("product_picture"));
+	         
+	         category.setCategorysNo(rs.getInt("category_no"));
+	         product.setCategorys(category);
+	         
+	         productList.add(product);
+	      }
+	         rs.close();
+	         pstmt.close();
+	         connection.close();
+	      
+	      return productList;
+	   }
+	
+	public Product selectProductByCategorys(int cateogorys_no) throws SQLException {
+		Product product = null;
+		String sql = "select P.product_no, P.product_name, P.product_price, P.product_discount_price, P.product_stock, P.Product_on_sale, "
+					+"		 P.category_no, C.categorys_name "
+					+"from product P, categorys C "
+					+"where P.category_no = C.categorys_no "
+					+"and P.category_no = ? ";
 		
 		Connection connection = getConnection();
 		PreparedStatement pstmt = connection.prepareStatement(sql);
-		pstmt.setInt(1, productNo);
+		pstmt.setInt(1, cateogorys_no);
 		ResultSet rs = pstmt.executeQuery();
+		if (rs.next()) {
+			product = rowToProduct(rs);
+		}
 		
-		while(rs.next()) {
+		rs.close();
+		pstmt.close();
+		connection.close();
+		
+		return product;
+	}
+	
+	/**
+	 * 모든 상품정보를 가져온다.
+	 * @return	상품정보
+	 * @throws SQLException
+	 */
+	public List<Product> selectAllProduct() throws SQLException {
+		List<Product> products = new ArrayList<>();
+		String sql = "select P.product_no, P.product_name, P.category_no, C.categorys_name, "
+				   + "P.product_price, P.product_discount_price, P.product_stock, P.product_on_sale "
+				   + "from product P, categorys C "
+				   + "where P.category_no = C.categorys_no "
+				   + "order by P.product_no asc ";
+		Connection connection = getConnection();
+		PreparedStatement pstmt = connection.prepareStatement(sql);
+		ResultSet rs = pstmt.executeQuery();
+		while (rs.next()) {
 			Product product = new Product();
-			Categorys category = new Categorys();
-			
 			product.setProductNo(rs.getInt("product_no"));
 			product.setProductName(rs.getString("product_name"));
 			product.setProductPrice(rs.getInt("product_price"));
 			product.setProductDiscountPrice(rs.getInt("product_discount_price"));
 			product.setProductStock(rs.getInt("product_stock"));
 			product.setProductOnSale(rs.getString("product_on_sale"));
-			product.setProductPicture(rs.getString("product_picture"));
 			
+			Categorys category = new Categorys();
 			category.setCategorysNo(rs.getInt("category_no"));
+			category.setCategorysName(rs.getNString("categorys_name"));
 			product.setCategorys(category);
 			
-			productList.add(product);
+			products.add(product);
 		}
-			rs.close();
-			pstmt.close();
-			connection.close();
-		
-		return productList;
-	}
-	
-	
-	/**
-	 * 모든 상품정보 가져오기
-	 * @return
-	 * @throws SQLException
-	 */
-	public List<Product> getAllProducts() throws SQLException {
-		String sql = "select product_no , category_no, product_name, product_price, "
-				+ "product_discount_price, product_stock, product_on_sale, product_picture "
-				+ "from product "
-				+ "where category_no = ? "
-				+ "order by product_no asc ";
-	
-	List<Product> productList = new ArrayList<>();
-	
-	Connection connection = getConnection();
-	PreparedStatement pstmt = connection.prepareStatement(sql);
-	ResultSet rs = pstmt.executeQuery();
-	while(rs.next()) {
-		Product product = new Product();
-		Categorys category = new Categorys();
-		
-		product.setProductNo(rs.getInt("product_no"));
-		product.setProductName(rs.getString("product_name"));
-		product.setProductPrice(rs.getInt("product_price"));
-		product.setProductDiscountPrice(rs.getInt("product_discount_price"));
-		product.setProductStock(rs.getInt("product_stock"));
-		product.setProductOnSale(rs.getString("product_on_sale"));
-		product.setProductPicture(rs.getString("product_picture"));
-		
-		category.setCategorysNo(rs.getInt("category_no"));
-		product.setCategorys(category);
-		
-		productList.add(product);
-	}
 		rs.close();
 		pstmt.close();
 		connection.close();
-	
-	return productList;
-	
+		
+		return products;
 	}
 	
 	/**
-	* 카테고리 번호에 해당하는 상품정보 가져오기
-	* @param categorysNo 카테고리번호
-	* @return 상품정보
-	* @throws SQLException
-	*/
-	public List<Product> getAllProductByCategorysNo(int categorysNo) throws SQLException {
-		String sql = "select product_no, category_no, product_name, product_price, "
-				+ "product_discount_price, product_stock, product_on_sale, product_picture "
-				+ "from product "
-				+ "where category_no = ? "
-				+ "order by product_no asc ";
-		List<Product> productList = new ArrayList<>();
-
-	Connection connection = getConnection();
-	PreparedStatement pstmt = connection.prepareStatement(sql);
-	pstmt.setInt(1, categorysNo);
-	ResultSet rs = pstmt.executeQuery();
-
-	while (rs.next()) {
-		Product product = new Product();
-		Categorys category = new Categorys();
+	 * 상품번호에 해당하는 상품정보를 가져온다.
+	 * @param productNo
+	 * @return	상품정보
+	 * @throws SQLException
+	 */
+	public Product selectProductByNo(int productNo) throws SQLException {
+		Product product = null;
+		String sql = "select P.product_no, P.product_name, P.product_price, P.product_discount_price, P.product_stock, P.Product_on_sale, "
+					+"		 P.category_no, C.categorys_name, P.product_picture "
+					+"from product P, categorys C "
+					+"where P.category_no = C.categorys_no "
+					+"and P.product_no = ? ";
 		
-		product.setProductNo(rs.getInt("product_no"));
-		product.setProductName(rs.getString("product_name"));
-		product.setProductPrice(rs.getInt("product_price"));
-		product.setProductDiscountPrice(rs.getInt("product_discount_price"));
-		product.setProductStock(rs.getInt("product_stock"));
-		product.setProductOnSale(rs.getString("product_on_sale"));
-		product.setProductPicture(rs.getString("product_picture"));
+		Connection connection = getConnection();
+		PreparedStatement pstmt = connection.prepareStatement(sql);
+		pstmt.setInt(1, productNo);
+		ResultSet rs = pstmt.executeQuery();
+		if (rs.next()) {
+			product = rowToProduct(rs);
+		}
 		
-		category.setCategorysNo(rs.getInt("category_no"));
-		product.setCategorys(category);
-
-		productList.add(product);
-	}
-
-	rs.close();
-	pstmt.close();
-	connection.close();
-
-	return productList;
-
+		rs.close();
+		pstmt.close();
+		connection.close();
+		
+		return product;
 	}
 	
 	/**
-	 * 상품 이름에 해당하는 상품정보를 가져온다
+	 * 상품 이름에 해당하는 상품정보를 가져온다.
 	 * @param productName 상품이름
 	 * @return 상품이름, null이 반환될 수 있다.
 	 * @throws SQLException
 	 */
 	public Product selectProductByName(String productName) throws SQLException {
 		Product product = null;
-		String sql = "select p.product_no, p.product_name, p.product_price, p.product_discount_price, p.product_stock, p.Product_on_sale, "
-					+"		 p.category_no, c.categorys_name "
-					+"from product p, categorys c "
-					+"where p.category_no = c.categorys_no "
+		String sql = "select P.product_no, P.product_name, P.product_price, P.product_discount_price, P.product_stock, P.Product_on_sale, "
+					+"		 P.category_no, C.categorys_name "
+					+"from product P, categorys C "
+					+"where P.category_no = C.categorys_no "
 					+"and P.product_name = ? ";
 		
 		Connection connection = getConnection();
@@ -182,8 +191,69 @@ public class ProductDao {
 		return product;
 	}
 	
+	
+	
 	/**
-	 * 귀찮은거 미리만들기
+	 * 새로운 상품을 저장한다.
+	 * @param product 상품정보
+	 * @throws SQLException
+	 */
+	public void insertProduct(Product product) throws SQLException{
+		String sql = "insert into product (product_no, product_name, category_no, product_price, product_discount_price, product_stock, product_on_sale, product_picture) "
+					+"values (product_no.nextval, ?, ?, ?, ?, ?, ?, ?) ";
+		
+		Connection connection = getConnection();
+		PreparedStatement pstmt = connection.prepareStatement(sql);
+		pstmt.setString(1, product.getProductName());
+		pstmt.setInt(2, product.getCategorys().getCategorysNo());
+		pstmt.setInt(3, product.getProductPrice());
+		pstmt.setInt(4, product.getProductDiscountPrice());
+		pstmt.setInt(5, product.getProductStock());
+		pstmt.setString(6, product.getProductOnSale());
+		pstmt.setString(7, product.getProductPicture());
+		
+		pstmt.executeUpdate();
+		
+		pstmt.close();
+		connection.close();
+	}
+	
+	
+	/**
+	 * 상품정보 업데이트 
+	 * @param product 상품정보
+	 * @throws SQLException
+	 */
+	public void updateProduct(Product product) throws SQLException {
+		String sql = "update product "
+					+" set "
+					+" 	product_name = ?, "
+					+" 	product_price = ?, "
+					+"  product_discount_price = ?, "
+					+"  product_stock = ?, "
+					+"  product_on_sale = ?, "
+					+"  product_picture = ? "
+					+" where product_no = ? ";
+		
+		Connection connection = getConnection();
+		PreparedStatement pstmt = connection.prepareStatement(sql);
+		pstmt.setString(1, product.getProductName());
+		pstmt.setInt(2, product.getProductPrice());
+		pstmt.setInt(3, product.getProductDiscountPrice());
+		pstmt.setInt(4, product.getProductStock());
+		pstmt.setString(5, product.getProductOnSale());
+		pstmt.setString(6, product.getProductPicture());
+		pstmt.setInt(7, product.getProductNo());
+		pstmt.executeUpdate();
+			
+		pstmt.close();
+		connection.close();
+	}
+					
+
+	
+	/**
+	 * 쓰기 귀찮은거 미리 만들어 두기
 	 * @param rs
 	 * @return
 	 * @throws SQLException
@@ -199,12 +269,20 @@ public class ProductDao {
 		product.setProductPicture(rs.getString("product_picture"));
 		
 		Categorys category = new Categorys();
-		category.setCategorysNo(rs.getInt("categorys_no"));
-		category.setCategorysName(rs.getNString("categorys_name"));
-		
+		category.setCategorysNo(rs.getInt("category_no"));
+		category.setCategorysName(rs.getString("categorys_name"));
 		product.setCategorys(category);
 		
 		return product;
 	}
-
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
