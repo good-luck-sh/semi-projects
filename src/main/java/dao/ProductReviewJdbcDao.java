50907506e6ee6bec0ae49bcbc07a9b99ca35b94d
초급개발 승히 <lion314@naver.com>
2021-12-05 16:21:03 +0900

    상품상세보기에서 작성된 리뷰 확인하기 구현함, 추가 구현 입력

--- src/main/java/dao/ProductReviewJdbcDao.java
+++ src/main/java/dao/ProductReviewJdbcDao.java
@@ -207,16 +207,16 @@ public class ProductReviewJdbcDao implements ProductReviewDao {
 		 		+ "P.product_no, P.category_no, P.product_name, P.product_price , P.product_discount_price, P.product_stock, P.product_on_sale, P.product_review_count, "
 		 		+ "P.product_star_point, P.product_date, P.product_picture "
 		 		+ "from review R, user_table U, product P, user_basket B "
-		 		+ "where R.user_no = U.user_no "
-		 		+ "and U.user_no = B.user_no "
-		 		+ "and P.product_no = B.product_no "
+		 		+ "where R.user_no = U.user_no(+) "
+		 		+ "and U.user_no = B.user_no(+) "
+		 		+ "and P.product_no(+) = B.product_no "
 		 		+ "and P.product_no =  ? ";
 		 Connection connection = getConnection();
 			PreparedStatement ptmt = connection.prepareStatement(sql);
 			ptmt.setInt(1, productNo);
 			ResultSet rs = ptmt.executeQuery();
 			
-			if(rs.next()) {
+			while(rs.next()) {
 				ProductDto productDto = new ProductDto();
 				productDto.setReviewNo(rs.getInt("review_no"));
 				productDto.setReviewTitle(rs.getString("review_title"));
